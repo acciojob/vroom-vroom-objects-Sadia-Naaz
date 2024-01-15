0933d2861d65c7b94f6f0f177982b9a5b5314fc0
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2024-01-15 19:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
+function Car(make, model) {
+this.make = make;
+this.model=model;
+__prototype__: function getMakeModel() {
+return this.make+""+this.model;
+}
+}
+Car();
+function SportsCar(make, model, topSpeed) {
+	this.make=make;
+	this.model=model;
+	this.topSpeed=topSpeed;
+__prototype__:Car();
+gettopSpeed(){
+	return this.topSpeed;
+}
+}
+const car = new Car("suv","new");
+const scar  =new SportsCar("honda",360,50"km/h");
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
